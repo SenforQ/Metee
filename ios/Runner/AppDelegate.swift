4c7cc8e1d15861cc89d807ffc32e392b86777594
senfor <senfor@guojiang.tv>
2025-07-18 14:04:46 +0800

    version 1.0.0 build 5

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,16 +8,6 @@ import AppTrackingTransparency
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       
-      
-      let arrayTempArray = [12,34,984,23]
-      for intnumber in arrayTempArray {
-          debugPrint("innumer : \(intnumber)")
-      }
-      let intnewNum = arrayTempArray[10]
-      let tempView = UIView()
-      tempView.backgroundColor = UIColor.white;
-      print("int Num : \(intnewNum)")
-
       DispatchQueue.main.asyncAfter(deadline: .now() + 3.6) {
           if #available(iOS 14, *) {
               ATTrackingManager.requestTrackingAuthorization { status in
